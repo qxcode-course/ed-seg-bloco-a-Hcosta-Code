fa244c0359caca2985128fe30eedfb554f94a9df
Henrique Costa <hcostacavalcante@gmail.com>
2026-03-16 16:21:39 -0300

    finalmente-consegui-codar-a-primeira

--- ed/pintando/go/draft.go
+++ ed/pintando/go/draft.go
@@ -1,5 +1,17 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-    fmt.Println("qxcode")
+	var a, b, c float64
+	fmt.Scanf("%f\n%f\n%f\n", &a, &b, &c)
+	var result float64
+
+	var p = (a + b + c) / 2
+	result = math.Sqrt(p * (p - a) * (p - b) * (p - c))
+
+	fmt.Printf("%.2f\n", result)
 }
