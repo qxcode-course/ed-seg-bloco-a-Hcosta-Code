b9baea6773f2b439b19af80dd380a1c4f76c5e2c
Henrique Costa <hcostacavalcante@gmail.com>
2026-03-16 23:59:03 -0300

    Fazendo-fuga

--- ed/fuga/go/draft.go
+++ ed/fuga/go/draft.go
@@ -1,5 +1,33 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
+
 func main() {
-    fmt.Println("qxcode")
+	var H, P, F, D int
+	fmt.Scanln(&H, &P, &F, &D)
+
+	for i := 0; i <= 15; i++ {
+		if D == -1 {
+			F++
+		} else {
+			F--
+		}
+
+		if F > 15 {
+			F = 0
+		} else if F < 0 {
+			F = 15
+		}
+
+		if F == H {
+			fmt.Print("N\n")
+			break
+		}
+		if F == P {
+			fmt.Print("S\n")
+			break
+		}
+	}
 }
