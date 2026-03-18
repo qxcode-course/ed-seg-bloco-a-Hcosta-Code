f1958492b5e38e358e0f5ede4a23a95ff4f3ea7c
Henrique Costa <hcostacavalcante@gmail.com>
2026-03-17 22:46:50 -0300

    Rapaz_passou,_mas_cheio_de_gambiarra

--- ed/baruel/go/draft.go
+++ ed/baruel/go/draft.go
@@ -8,4 +8,28 @@ func main() {
 	fmt.Scan(&qtd_album, &qtd_fig)
 	album := make([]int, qtd_fig)
 
+	unicos := make(map[int]bool)
+	repetidos := make([]int, 0, qtd_fig)
+
+	for i := range album {
+		fmt.Scan(&album[i])
+	}
+
+	for _, fig := range album {
+		if unicos[fig] {
+			repetidos = append(repetidos, fig)
+		} else {
+			unicos[fig] = true
+		}
+
+	}
+
+	for i := range repetidos {
+		if i != 0 {
+			fmt.print(" ")
+		}
+		fmt.Printf("%v ", valor)
+	}
+	fmt.Print("\n")
+
 }
--- ed/pedra/go/draft.go
+++ ed/pedra/go/draft.go
@@ -7,16 +7,46 @@ import (
 func main() {
 	var N int
 	fmt.Scanln(&N)
+	var desclass = 0
+	var indice int = 0
 
 	type Player struct {
-		A int
-		B int
+		A   int
+		B   int
+		Dif int
 	}
 
 	pontos := make([]Player, N)
 
-    for var i int = 0; i < N; i++ {
-        fmt.Scanln(pontos[i].A, pontos[i].B)
-    }
+	for i := 0; i < N; i++ {
+		fmt.Scanln(&pontos[i].A, &pontos[i].B)
+		if pontos[i].A >= 10 && pontos[i].B >= 10 {
+			pontos[i].Dif = pontos[i].A - pontos[i].B
+		} else if pontos[i].A < 10 || pontos[i].B < 10 {
+			desclass++
+			pontos[i].Dif = 10000
+		} else {
+			pontos[i].Dif = 10000
+		}
+	}
 
+	for i := 0; i < N; i++ {
+		if pontos[i].Dif < 0 {
+			pontos[i].Dif = pontos[i].Dif * -1
+		}
+	}
+
+	for i := 1; i < N; i++ {
+		if pontos[indice].Dif == pontos[i].Dif {
+			continue
+		} else if pontos[indice].Dif > pontos[i].Dif {
+			indice = i
+		}
+	}
+
+	if desclass == N {
+		fmt.Print("sem ganhador\n")
+	} else {
+		fmt.Printf("%v\n", indice)
+	}
 }
