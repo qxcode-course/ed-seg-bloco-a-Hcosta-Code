14297dad365fe42827dedd30a4327b2a789d2a7c
Henrique Costa <henriquefacull@gmail.com>
2026-06-11 03:13:41 -0300

    aaaa

--- ed/prin_list1/src/go/main.go
+++ ed/prin_list1/src/go/main.go
@@ -7,18 +7,29 @@ import (
 
 // mostra a lista com o elemento sword destacado
 func ToStr(l *DList[int], sword *DNode[int]) string {
-	return ""
+	anotaao := []string{}
+	for it := l.Front(); it != l.End(); it = it.Next() {
+		if it == sword {
+			anotaao = append(anotaao, fmt.Sprintf("%d>", it.Value))
+		} else {
+			anotaao = append(anotaao, fmt.Sprintf("%d", it.Value))
+		}
+	}
+
+	return "[ " + strings.Join(anotaao, " ") + " ]"
 }
 
 // move para frente na lista circular
 func Next(l *DList[int], it *DNode[int]) *DNode[int] {
-	return nil
+	if it.Next() == l.End() {
+		return l.Front()
+	}
+	return it.Next()
 }
 
 func main() {
 	var qtd, chosen int
 	fmt.Scan(&qtd, &chosen)
-	fmt.Println(qtd, chosen)
 	l := NewDList[int]()
 	for i := 1; i <= qtd; i++ {
 		l.PushBack(i)
